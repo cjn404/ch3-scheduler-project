c629b9d3fcd83c751918961d46806592ae7295e9
cjn404 <moi404@naver.com>
2025-08-02 15:26:12 +0900

    feat: add entity in lv1 for schedule creation

--- src/main/java/org/example/ch3schedulerapiproject/scheduler/entity/Scheduler.java
+++ src/main/java/org/example/ch3schedulerapiproject/scheduler/entity/Scheduler.java
@@ -1,4 +1,29 @@
 package org.example.ch3schedulerapiproject.scheduler.entity;
 
-public class Scheduler {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+public class Scheduler extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String password;
+    private String name;
+    private String title;
+    private String content;
+
+    public Scheduler(String password, String name, String title, String content) {
+        this.password = password;
+        this.name = name;
+        this.title = title;
+        this.content = content;
+    }
 }
