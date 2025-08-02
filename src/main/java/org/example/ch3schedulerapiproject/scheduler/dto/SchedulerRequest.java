b2b3d79bf4575bb885d985efb1b72ac8ddedeb19
cjn404 <moi404@naver.com>
2025-08-02 15:35:56 +0900

    feat: add dto in lv1 for schedule creation

--- src/main/java/org/example/ch3schedulerapiproject/scheduler/dto/SchedulerRequest.java
+++ src/main/java/org/example/ch3schedulerapiproject/scheduler/dto/SchedulerRequest.java
@@ -1,4 +1,12 @@
 package org.example.ch3schedulerapiproject.scheduler.dto;
 
+import lombok.Getter;
+
+@Getter
 public class SchedulerRequest {
+
+    private String password;
+    private String name;
+    private String title;
+    private String content;
 }
--- src/main/java/org/example/ch3schedulerapiproject/scheduler/dto/SchedulerResponse.java
+++ src/main/java/org/example/ch3schedulerapiproject/scheduler/dto/SchedulerResponse.java
@@ -1,4 +1,32 @@
 package org.example.ch3schedulerapiproject.scheduler.dto;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class SchedulerResponse {
+
+    private final Long id;
+    private final String name;
+    private final String title;
+    private final String content;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+
+    public SchedulerResponse(
+            Long id,
+            String name,
+            String title,
+            String content,
+            LocalDateTime createdAt,
+            LocalDateTime modifiedAt
+    ) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
 }
