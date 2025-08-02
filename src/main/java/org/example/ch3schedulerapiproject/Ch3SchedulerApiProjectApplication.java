cf695a105f12bcd808e662d5d268e15c55c1ba89
cjn404 <moi404@naver.com>
2025-08-02 18:03:50 +0900

    fix: add @EnableJpaAuditing to fix timestamp in lv1 for schedule creation

--- src/main/java/org/example/ch3schedulerapiproject/Ch3SchedulerApiProjectApplication.java
+++ src/main/java/org/example/ch3schedulerapiproject/Ch3SchedulerApiProjectApplication.java
@@ -2,7 +2,9 @@ package org.example.ch3schedulerapiproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Ch3SchedulerApiProjectApplication {
 
